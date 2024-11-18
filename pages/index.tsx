ea31f9097175d36ae834cb605c1dda44356fcd7a
Jayson Zou <1120957260@qq.com>
2024-11-18 14:10:10 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,27 +7,15 @@ function HomePage() {
       <Container>
         <div className="space-y-6">
           <h1 className="text-2xl font-bold">
-            Hey, I'm a Senior Software Engineer at Company. I enjoy working with
-            Next.js and crafting beautiful front-end experiences.
+             This is where I keep track of things.
           </h1>
           <p>
-            This portfolio is built with Next.js and a library called next-mdx.
-            It allows you to write Markdown and focus on the content of your
-            portfolio.
+            世上无难事，只要肯登攀。
           </p>
-
-          <p>Deploy your own in a few minutes.</p>
         </div>
       </Container>
 
-      <div className="container max-w-4xl m-auto px-4 mt-20">
-        <Image
-          src="/desk.jpg"
-          alt="my desk"
-          width={1920 / 2}
-          height={1280 / 2}
-        />
-      </div>
+      
     </>
   );
 }
